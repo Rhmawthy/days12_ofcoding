89e331fb103d923f32ee707aada33e5b8184788f
Rahmawati <115470125+Rhmawthy@users.noreply.github.com>
2022-10-22 05:24:17 -0700

    Add files via upload

---
+++ days12_ for loop.py
@@ -0,0 +1,39 @@
+# loop(perulangan )
+
+#contoh cara kerja perulangan dalam kelipatan 3
+#loop 1  angka = 3
+#loop 2  angka = angka + 3
+             # print(angka)
+#loop 3  angka = angka + 3
+             # print (angka)
+#loop seterusnya......
+
+#perulangan di bedakan menjadi 2  yaitu:
+# perulangan for ( for loop )
+# for dalam fungsi range()
+for i in range (7): # 
+    print ( "angka i ke",i)
+
+
+#contoh kasus melakukan list pada nama kabupaten di sulbar dalam for tuple
+print(70*"=")
+
+nama_kab = ( "mateng","mamuju","majene","polman","mamasa" )
+for kab in nama_kab :
+    print ("nama kab",kab)
+
+#for dengan string
+print(70*"=")
+
+nama = "RAHMAWATI"
+for karakter in nama:
+    print(karakter)
+
+
+
+
+
+
+  
+
+
